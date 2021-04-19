1a9cdad6b4e412c5bbe66231c8c13872cfe02768
Brian Reich <reichman213@gmail.com>
2021-04-19 12:20:31 -0500

    spotify auth scopes

---
+++ api/src/utils/scopes.ts
@@ -0,0 +1,19 @@
+const scopes = [
+    'user-read-recently-played',
+    'user-top-read',
+    'user-read-playback-position',
+
+    'user-read-playback-state',
+    'user-modify-playback-state',
+    'user-read-currently-playing',
+
+    'playlist-read-private',
+    'playlist-read-collaborative',
+    
+    'user-library-read',
+
+    'user-read-email',
+    'user-read-private',
+];
+
+export default scopes;
